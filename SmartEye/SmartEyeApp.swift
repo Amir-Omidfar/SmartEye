2793536712d10145cab84831c412d3f351880dec
Amir Omidfar <omidfar73@gmail.com>
2025-09-10 21:15:04 -0700

    Initial Commit

---
+++ SmartEye/SmartEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartEyeApp.swift
+//  SmartEye
+//
+//  Created by Amir Ali on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartEyeTests/SmartEyeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartEyeTests.swift
+//  SmartEyeTests
+//
+//  Created by Amir Ali on 9/10/25.
+//
+
+import Testing
+@testable import SmartEye
+
+struct SmartEyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
